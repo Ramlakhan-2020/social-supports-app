feed7b2de5ce80681b0e3fe56bccfa43f9fb9547
Ramlakhan-2020 <ramlakhansingh38@gmail.com>
2025-07-29 10:12:56 +0530

    add customize talilwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     ],
     theme: {
       extend: {},
+      // screens: {
+      //   xs:"400px",     // customize
+      //   sm: '640px',   // Small devices (mobile)
+      //   md: '768px',   // Medium devices (tablet)
+      //   lg: '1024px',  // Large devices (laptop)
+      //   xl: '1280px',  // Extra large devices (desktop)
+      //   "2xl": '1536px' // customize
+      // }
     },
     plugins: [],
   }
